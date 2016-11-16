98adad8c45a645d1b77a137f932d4948607d375b
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2016-11-16 11:01:03 -0500

    Add teams to leagues

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,3 +1,11 @@
 class League < ActiveRecord::Base
   has_many :conferences
+  
+  def divisions
+    conferences.map { |conference| conference.divisions }.flatten
+  end
+  
+  def teams
+    divisions.map { |division| division.teams }.flatten
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   resources :leagues, only: [:index, :show]
+  get "/leagues/:id/teams", to: "leagues#show_teams"
 end
