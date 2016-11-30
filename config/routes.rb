44e348cb63579a5c663e8434577ed12d105bf850
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2016-11-30 16:21:40 -0500

    Add sessions controller and new session template

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :leagues, only: [:index, :show]
   get "/leagues/:id/teams", to: "leagues#show_teams"
   get "/schedule/:team/:season", to: "schedules#schedule"
+  get "/login", to: "sessions#new"
 end
